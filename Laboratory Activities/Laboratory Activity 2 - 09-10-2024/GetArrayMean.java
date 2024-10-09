3d5b41ffb230528159db34797d8b1fc852618b36
ailadonayre <163071891+ailadonayre@users.noreply.github.com>
2024-10-09 13:13:25 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 2 - 09-10-2024/GetArrayMean.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GetArrayMean {
+
+    public static float getArrayMean(int[] arr) {
+        int sum = 0;
+
+        for (int num : arr) {
+            sum += num;
+        }
+
+        return (float) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nEnter number of elements: ");
+        int n = scanner.nextInt();
+        int[] array_num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number: ");
+            array_num[i] = scanner.nextInt();
+        }
+
+        float mean = getArrayMean(array_num);
+        System.out.printf("Mean of array is: %.2f%n", mean);
+    }
+}
